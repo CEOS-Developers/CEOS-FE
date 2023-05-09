13b80024b01c551887986f743d3b2b85b4f0aaf7
kongnayeon <seoultownroad@naver.com>
2023-05-09 23:56:05 +0900

    feat: 프리텐다드 추가

--- apps/admin/src/pages/_document.tsx
+++ apps/admin/src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
