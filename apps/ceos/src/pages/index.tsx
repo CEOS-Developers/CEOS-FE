91199e97e5c2e846511aab98aca24dc081873b77
juhyojeong <juhyojeong58@gmail.com>
2023-05-11 21:13:33 +0900

    fix: 색상 및 스타일 수정

--- apps/ceos/src/pages/index.tsx
+++ apps/ceos/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@ceos-fe/ui";
+import { Button, TextField } from '@ceos-fe/ui';
 
 export default function Home() {
   return (
@@ -6,7 +6,14 @@ export default function Home() {
       <div>
         ceos
         <Button />
-        <TextField width="s" />
+        <TextField
+          width={372}
+          label="제목"
+          helperText={[
+            { type: 'normal', text: '일반 텍스트' },
+            { type: 'important', text: '중요 텍스트' },
+          ]}
+        />
       </div>
     </>
   );
