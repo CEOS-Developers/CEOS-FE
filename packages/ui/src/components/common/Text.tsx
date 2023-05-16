cea4c33b31332f0760f72cb1315d33023853aa93
Jieun Oh <0909oje@naver.com>
2023-05-16 20:03:51 +0900

    feat: 공통으로 처리 가능한 Text 추가

--- packages/ui/src/components/common/Text.tsx
+++ packages/ui/src/components/common/Text.tsx
@@ -2,6 +2,22 @@ import styled from '@emotion/styled';
 import { WebOfTypo, MobileOfTypo, theme } from '../../styles';
 import { KeyOfPalette } from '../../styles';
 
+export const Text = styled.div<{
+  webTypo: WebOfTypo;
+  mobileTypo: MobileOfTypo;
+  color: KeyOfPalette;
+}>`
+  ${({ webTypo }) => theme.typo.Web[webTypo]};
+  color: ${({ color }) => theme.palette[color]};
+  display: flex;
+  align-items: center;
+
+  /* 브라우저 크기에 따라 가로 크기 변경 */
+  @media (max-width: 1023px) {
+    ${({ mobileTypo }) => theme.typo.Mobile[mobileTypo]};
+  }
+`;
+
 export const WebText = styled.div<{
   webTypo: WebOfTypo;
   color: KeyOfPalette;
@@ -15,7 +31,6 @@ export const WebText = styled.div<{
 export const MobileText = styled.div<{
   mobileTypo: MobileOfTypo;
   color: KeyOfPalette;
-  // height: number;
 }>`
   ${({ mobileTypo }) => theme.typo.Mobile[mobileTypo]};
   color: ${({ color }) => theme.palette[color]};
