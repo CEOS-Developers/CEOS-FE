b40b0503086f7068c340c2e9efc250a042f7d37a
Jieun Oh <0909oje@naver.com>
2023-05-16 22:13:26 +0900

    feat: 운영진&멘토 카드 작업

--- packages/ui/src/components/Card/ManagementCard.tsx
+++ packages/ui/src/components/Card/ManagementCard.tsx
@@ -1,12 +1,14 @@
-import styled from "@emotion/styled";
-import { theme } from "../../styles";
-import { Text } from "../common";
-import { EmotionJSX } from "@emotion/react/types/jsx-namespace";
+import styled from '@emotion/styled';
+import { theme } from '../../styles';
+import { Text } from '../common';
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
+import { useState } from 'react';
 
 export interface IManagementCard {
   img?: string;
   position: string;
   name: string;
+  univ: string;
   dept: string;
   explain?: string[];
 }
@@ -14,18 +16,21 @@ export interface IManagementCard {
 export const ManagementCard = (props: {
   managementCard: IManagementCard;
 }): EmotionJSX.Element => {
-  const { img, position, name, dept, explain } = props.managementCard;
+  const { img, position, name, univ, dept, explain } = props.managementCard;
+
   return (
     <Wrapper>
       {img ? <Profile src={img} /> : <></>}
       <Content>
-        <Text typo="Label3" color="Gray5">
+        <Text webTypo="Label3" mobileTypo="Label2" color="Gray5">
           {position}
         </Text>
-        <Text typo="Label3" color="Black">
+        <Text webTypo="Heading4" mobileTypo="Heading3" color="Black">
           {name}
         </Text>
-        <Text typo="Body3" color="Gray5">
+        <Text className="ll" webTypo="Body3" mobileTypo="Body2" color="Gray5">
+          {univ + ' '}
+          <br />
           {dept}
         </Text>
       </Content>
@@ -36,20 +41,33 @@ export const ManagementCard = (props: {
 export const MentorCard = (props: {
   mentorCard: IManagementCard;
 }): EmotionJSX.Element => {
-  const { img, position, name, dept, explain } = props.mentorCard;
+  const { img, position, name, univ, dept, explain } = props.mentorCard;
   return (
-    <Wrapper className={explain ? "non-hover" : ""}>
-      <Content>
-        <Text typo="Label3" color="Gray5">
+    <Wrapper className={explain ? 'non-hover' : ''}>
+      <Content className={explain ? 'non-hover' : ''}>
+        <Text webTypo="Label3" mobileTypo="Label2" color="Gray5">
           {position}
         </Text>
-        <Text typo="Label3" color="Black">
+        <Text webTypo="Heading4" mobileTypo="Heading3" color="Black">
           {name}
         </Text>
-        <Text typo="Body3" color="Gray5">
+        <Text webTypo="Body3" mobileTypo="Body2" color="Gray5">
+          {univ + ' '}
+          <br />
           {dept}
         </Text>
       </Content>
+      {explain ? (
+        <Content className="is-hover">
+          {explain.map((ex) => (
+            <Text webTypo="Body3" mobileTypo="Body1" color="White">
+              {ex}
+            </Text>
+          ))}
+        </Content>
+      ) : (
+        <></>
+      )}
     </Wrapper>
   );
 };
@@ -59,13 +77,20 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 30px 26px;
-
   width: 272px;
 
-  &:hover {
-    .mentor {
-      background-color: #3e4cf7cc;
-      opacity: 0.8;
+  & > .is-hover {
+    display: none;
+  }
+
+  .non-hover&:hover {
+    background-color: #3e4cf7;
+    opacity: 0.8;
+    & > .non-hover {
+      display: none;
+    }
+    & > .is-hover {
+      display: block;
     }
   }
 
@@ -74,8 +99,13 @@ const Wrapper = styled.div`
 
   /* 브라우저 크기에 따라 가로 크기 변경 */
   @media (max-width: 1023px) {
-  }
-  @media (max-width: 767px) {
+    width: 166px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    gap: 14px;
   }
 `;
 
@@ -84,11 +114,27 @@ const Content = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  margin-top: 20px;
   height: 102px;
+
+  & > * > br {
+    display: none;
+  }
+
+  @media (max-width: 1023px) {
+    height: 112px;
+    & > * > br {
+      display: inline;
+    }
+  }
 `;
 
 const Profile = styled.img`
   width: 150px;
   height: 150px;
+  margin-bottom: 20px;
+  @media (max-width: 1023px) {
+    margin-bottom: 14px;
+    width: 90px;
+    height: 90px;
+  }
 `;
--- packages/ui/src/components/common/Wrapper.tsx
+++ packages/ui/src/components/common/Wrapper.tsx
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Row = styled.div`
   display: flex;
