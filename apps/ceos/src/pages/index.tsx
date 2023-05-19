139ab309ad2c2efcb46e452293da8aae8fc48c83
juhyojeong <juhyojeong58@gmail.com>
2023-05-19 19:12:47 +0900

    feat: 예시 필드 구현

--- apps/ceos/src/pages/index.tsx
+++ apps/ceos/src/pages/index.tsx
@@ -4,16 +4,72 @@ import { Header } from '@ceos/components/Header';
 import { FAQBox } from '@ceos/components/FAQBox';
 
 export default function Home() {
-  const { register } = useForm({
+  const { register, getValues } = useForm({
     defaultValues: {
-      title: '',
-      content: '',
+      reward: 'Healthier',
+      rewardDescription: '온라인 감별진단 ・ 건강관리 서비스',
+      submissionSmall: '이름',
+      submissionSmallExtension:
+        '이번 학기 세오스 활동 외 어떤 활동을 하는지 간략히 적어주세요.',
+      submissionMedium:
+        '1. CEOS는 다양한 사람들과 파트가 모여 프로젝트를 만들어갑니다. 이처럼 다양한 사람들과의 협업 만족도가 높은 프로덕트를 만들기 위해, UXUI 디자이너로서 본인의 역할은 무엇이라고 생각하는지 서술해주세요. (200자 내외)',
+      submissionSet:
+        '이번 학기 세오스 활동 외 어떤 활동을 하는지 간략히 적어주세요.',
+      submissionSetDescription: '내용을 입력해주세요',
     },
   });
   const colors = ['Green', 'Skyblue', 'Yellow'];
   return (
-    <Flex direction="column">
-      <RemovableTextField width={1092} />
+    <Flex direction="column" webGap={20}>
+      {/* 리워드 전용 */}
+      <Flex width={1271}>
+        <TextField {...register('reward')} />
+        <RemovableTextField
+          {...register('rewardDescription')}
+          width={772}
+          gap={28}
+          handleClick={() => console.log(getValues())}
+        />
+      </Flex>
+
+      {/* 질문지 전용 */}
+      <RemovableTextField
+        {...register('submissionSmall')}
+        width={292}
+        gap={12}
+        handleClick={() => console.log(getValues())}
+      />
+
+      <RemovableTextField
+        {...register('submissionSmallExtension')}
+        width={1092}
+        gap={12}
+        handleClick={() => console.log(getValues())}
+      />
+
+      <RemovableTextField
+        {...register('submissionMedium')}
+        width={1092}
+        height={78}
+        multiline
+        gap={12}
+        handleClick={() => console.log(getValues())}
+      />
+
+      <Flex direction="column" webGap={20}>
+        <RemovableTextField
+          {...register('submissionSet')}
+          width={1092}
+          gap={12}
+          handleClick={() => console.log(getValues())}
+        />
+        <TextField
+          {...register('submissionSetDescription')}
+          width={1171}
+          height={120}
+          multiline
+        />
+      </Flex>
     </Flex>
   );
 }
--- packages/ui/src/components/TextField/RemovableTextField.tsx
+++ packages/ui/src/components/TextField/RemovableTextField.tsx
@@ -7,8 +7,10 @@ import styled from '@emotion/styled';
 interface RemovableTextFieldProps
   extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
   width?: number;
+  height?: number;
   gap?: number;
   multiline?: boolean;
+  handleClick: () => void;
 }
 
 /**
@@ -24,16 +26,25 @@ export const RemovableTextField = forwardRef<
     {
       placeholder = '내용을 입력해주세요.',
       width = 372,
+      height = 47,
       gap = 12,
       multiline = false,
+      handleClick,
       ...props
     },
     ref,
   ) => {
     return (
-      <Flex webGap={gap}>
-        <TextField ref={ref} width={width} {...props} />
-        <ButtonBox>
+      <Flex webGap={gap} align="flex-start">
+        <TextField
+          ref={ref}
+          width={width}
+          height={height}
+          multiline={multiline}
+          placeholder={placeholder}
+          {...props}
+        />
+        <ButtonBox onClick={handleClick}>
           <Text webTypo="Body2" color="Gray5">
             삭제
           </Text>
