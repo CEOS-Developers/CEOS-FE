d171853bcc4b3187214a48e5b5333c70387d429f
kongnayeon <seoultownroad@naver.com>
2023-05-20 18:39:07 +0900

    fix: svg 삭제

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import * as React from 'react';
 export * from './src/components/Button';
 export * from './src/components/TextField';
 export * from './src/styles/';
-=======
-import * as React from "react";
-export * from "./src/components/Button";
-export * from "./src/components/TextField";
-export * from "./src/styles/";
->>>>>>> dev
