3135019613e01189a070b325fd5a0d5161fda8bc
Jieun Oh <0909oje@naver.com>
2023-05-24 23:30:58 +0900

    feat: Admin 영역 겹치기용 Container & Flex

--- packages/ui/src/components/common/Wrapper.tsx
+++ packages/ui/src/components/common/Wrapper.tsx
@@ -9,6 +9,7 @@ export const Flex = styled.div<{
   mobileGap?: number;
   width?: number;
   height?: number;
+  borderRadius?: number;
 }>`
   display: flex;
   flex-direction: ${({ direction }) => (direction ? `${direction}` : 'row')};
@@ -18,9 +19,33 @@ export const Flex = styled.div<{
   width: ${({ width }) => (width ? `${width}px` : '100%')};
   height: ${({ height }) => (height ? `${height}px` : '100%')};
   margin: ${({ margin }) => (margin ? margin : '0')};
+  border-radius: ${({ borderRadius }) =>
+    borderRadius ? `${borderRadius}px` : '0px'};
 
   /* 브라우저 크기에 따라 가로 크기 변경 */
   @media (max-width: 1023px) {
     gap: ${({ mobileGap }) => (mobileGap ? `${mobileGap}px` : '0px')};
+  }
+`;
+
+export const AdminContainer = styled(Flex)`
+  position: relative;
+  .is-hover {
+    display: none;
+  }
+  :hover {
+    .is-hover {
+      display: flex;
+    }
+  }
+`;
+
+export const AdminFlex = styled(Flex)`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  &.is-hover {
+    background-color: rgba(0, 0, 0, 0.5);
   }
 `;
