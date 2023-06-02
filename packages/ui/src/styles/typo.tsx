971e5c92e8d2f23d922c88966bc8dc2bf1d1e859
juhyojeong <juhyojeong58@gmail.com>
2023-06-02 13:19:32 +0900

    refactor: typo 스타일 수정

--- packages/ui/src/styles/typo.tsx
+++ packages/ui/src/styles/typo.tsx
@@ -6,67 +6,67 @@ export const typo = {
   Web: {
     Heading1_Eng: css`
       font-family: 'Gilroy-Bold', 'Apple SD Gothic Neo';
-      font-size: ${calcRem(40)};
+      font-size: ${calcRem(36)};
       line-height: 122.5%;
     `,
     Heading1_Kor: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(40)};
+      font-size: ${calcRem(36)};
       line-height: 120%;
     `,
     Heading2: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(34)};
+      font-size: ${calcRem(24)};
       line-height: 150%;
     `,
     Heading3: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(24)};
+      font-size: ${calcRem(20)};
       line-height: 150%;
     `,
     Heading4: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(22)};
+      font-size: ${calcRem(18)};
       line-height: 170%;
     `,
     Body1: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 500;
-      font-size: ${calcRem(20)};
+      font-size: ${calcRem(18)};
       line-height: 170%;
     `,
     Body2: css`
       font-family: 'Pretendar', 'Apple SD Gothic Neo';
       font-weight: 500;
-      font-size: ${calcRem(18)};
+      font-size: ${calcRem(16)};
       line-height: 170%;
     `,
     Body3: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 500;
-      font-size: ${calcRem(16)};
+      font-size: ${calcRem(14)};
       line-height: 170%;
     `,
     Label1: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(20)};
+      font-size: ${calcRem(18)};
       line-height: 120%;
     `,
     Label2: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(18)};
+      font-size: ${calcRem(16)};
       line-height: 170%;
     `,
     Label3: css`
       font-family: 'Pretendard', 'Apple SD Gothic Neo';
       font-weight: 600;
-      font-size: ${calcRem(16)};
+      font-size: ${calcRem(14)};
       line-height: 150%;
     `,
   },
