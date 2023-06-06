49eecf23660e24a47b638e158177c47fd77fd043
juhyojeong <juhyojeong58@gmail.com>
2023-06-06 15:03:28 +0900

    refactor: Dropdown 통합

--- apps/admin/src/components/Dropdown/index.tsx
+++ apps/admin/src/components/Dropdown/index.tsx
@@ -1,2 +1,120 @@
-export * from './Dropdown';
-export * from './PassDropdown';
+import { LargeArrowDown, LargeArrowUp } from '@admin/assets/Arrow';
+import { Flex, theme } from '@ceos-fe/ui';
+import styled from '@emotion/styled';
+import { useModal } from '@ceos-fe/utils';
+import { FieldValues, UseFormSetValue } from 'react-hook-form';
+
+interface DropdownItemInterface {
+  label: string;
+  value: string;
+  color?: string;
+  background?: string;
+}
+interface DropdownProps {
+  options: DropdownItemInterface[];
+  label: string;
+  value: DropdownItemInterface;
+  setValue: UseFormSetValue<FieldValues>;
+  placeholder: string;
+  width?: number;
+}
+
+/**
+ * @param {{ label: string; value: string; color?: string; background?: string; }[]} options: dropdown option 목록
+ * @param {string} label: dropdown 고유 label
+ * @param { label: string; value: string; color: string; background: string; } value: 현재 선택된 값
+ * @param { UseFormSetValue } setValue: dropdown 값 변경을 위한 함수
+ * @param {string} placeholder: 선택되지 않은 상태의 텍스트
+ * @param {number} width: Dropdown width
+ */
+export const Dropdown = ({
+  options,
+  label,
+  value,
+  setValue,
+  placeholder,
+  width = 89,
+}: DropdownProps) => {
+  const { isOpen, modalRef, toggleModal } = useModal();
+  const color =
+    value && value.color && !isOpen ? value.color : theme.palette.Admin.Navy;
+  const backgroundColor =
+    value && value.background && !isOpen
+      ? value.background
+      : theme.palette.White;
+
+  return (
+    <Container ref={modalRef} onClick={toggleModal} width={width}>
+      <DropdownList background={backgroundColor} color={color}>
+        <DropdownButton width={width} borderRadius={6} justify="space-between">
+          <DropdownLabel color={color}>
+            {value && !isOpen ? value.label : placeholder}
+          </DropdownLabel>
+          <ArrowContainer>
+            {isOpen ? (
+              <LargeArrowUp color={color} />
+            ) : (
+              <LargeArrowDown color={color} />
+            )}
+          </ArrowContainer>
+        </DropdownButton>
+
+        {isOpen && (
+          <>
+            {options.map((opt, idx) => (
+              <DropdownItem key={idx} onClick={() => setValue(label, opt)}>
+                {opt.label}
+              </DropdownItem>
+            ))}
+          </>
+        )}
+      </DropdownList>
+    </Container>
+  );
+};
+
+const Container = styled.div<{ width: number }>`
+  z-index: 5;
+
+  width: ${({ width }) => width}px;
+  height: 34px;
+`;
+const ArrowContainer = styled.div`
+  position: absolute;
+  right: 26px;
+`;
+const DropdownButton = styled(Flex)`
+  position: relative;
+
+  box-sizing: border-box;
+  cursor: pointer;
+`;
+const DropdownLabel = styled.p<{ color: string }>`
+  font-family: 'Pretendard', 'Apple SD Gothic Neo';
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+  color: ${({ color }) => color};
+`;
+const DropdownList = styled.ul<{
+  color: string;
+  background: string;
+}>`
+  box-sizing: border-box;
+
+  width: 100%;
+  padding: 8px 16px;
+
+  background-color: ${({ background }) => background};
+  border: 1px solid ${({ color }) => color};
+  border-radius: 6px;
+`;
+const DropdownItem = styled.li`
+  margin-top: 6px;
+
+  ${theme.typo.Mobile.Body2}
+  color: ${theme.palette.Gray4};
+  text-align: left;
+
+  cursor: pointer;
+`;
--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -1,91 +1,32 @@
-import { DataGrid } from '@admin/components/DataGrid';
-import { useState } from 'react';
-import { PageInterface } from '@admin/components/DataGrid/Pagination';
-import styled from '@emotion/styled';
-import { Flex } from '@ceos-fe/ui';
-import { ExtraButton } from '@admin/components/ExtraButton';
+import { Dropdown } from '@admin/components/Dropdown';
+import { useForm } from 'react-hook-form';
+import { theme } from '@ceos-fe/ui';
 
 export default function Home() {
-  const [pagination, setPagination] = useState<PageInterface>({
-    page: 1, // 현재 선택된 페이지 넘버
-    pageSize: 10, // 한 페이지에 들어가는 데이터 개수
-    total: 10, // 전체 페이지 개수
-  });
-  const [dataSource, setDataSource] = useState<object[]>(
-    Array.from(new Array(10), (_, i) => {
-      return {
-        key: i,
-        name: `주효정${i + 1}`,
-        part: '프론트',
-        email: '2713jhj@naver.com',
-        auth: '일반유저',
-      };
-    }),
-  );
-
-  const columns = [
-    {
-      title: '이름',
-      dataIndex: 'name',
-      width: '271px',
-    },
-    {
-      title: '직군',
-      dataIndex: 'part',
-      width: '301px',
-    },
-    {
-      title: '이메일',
-      dataIndex: 'email',
-    },
-    {
-      title: '권한',
-      dataIndex: 'auth',
-      width: '300px',
-      render: () => (
-        <Flex justify="space-between">
-          <p>일반유저</p>
-          <ExtraButton
-            buttonList={[
-              { label: '수정하기', handleClick: () => console.log('수정') },
-              { label: '삭제하기', handleClick: () => console.log('삭제') },
-            ]}
-          />
-        </Flex>
-      ),
-    },
-  ];
-
-  const onChangePage = (newPage: number) => {
-    setPagination({ ...pagination, page: newPage });
-    setDataSource(
-      Array.from(new Array(10), (_, i) => {
-        return {
-          key: i,
-          name: `주효정${newPage * 10 + i + 1}`,
-          part: '프론트',
-          email: '2713jhj@naver.com',
-          auth: '일반유저',
-        };
-      }),
-    );
-  };
+  const { setValue, watch } = useForm();
 
   return (
-    <Container>
-      <DataGrid
-        pagination={pagination}
-        dataSource={dataSource}
-        columns={columns}
-        onChangePage={onChangePage}
+    <>
+      <Dropdown
+        options={[
+          {
+            label: '합격',
+            value: 'pass',
+            background: theme.palette.Admin.Green2,
+            color: theme.palette.Admin.Green1,
+          },
+          {
+            label: '불합격',
+            value: 'nonpass',
+            background: theme.palette.Admin.Red2,
+            color: theme.palette.Admin.Red1,
+          },
+        ]}
+        label="passDropdown"
+        setValue={setValue}
+        value={watch('passDropdown')}
+        placeholder="선택"
       />
-    </Container>
+    </>
   );
 }
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  position: relative;
-`;
