2d3d08f834eb959870b74d121c4868085b906d7a
Jieun Oh <0909oje@naver.com>
2023-06-19 16:04:59 +0900

    feat: ceos 페이지 구조 잡기

---
+++ apps/ceos/src/pages/FAQ/index.tsx
@@ -0,0 +1,7 @@
+import { Flex } from '@ceos-fe/ui';
+
+const FAQ = () => {
+  return <Flex direction="row">Activity 페이지</Flex>;
+};
+
+export default FAQ;
---
+++ apps/ceos/src/pages/project/index.tsx
@@ -0,0 +1,7 @@
+import { Flex } from '@ceos-fe/ui';
+
+const Project = () => {
+  return <Flex direction="row">Project 페이지</Flex>;
+};
+
+export default Project;
