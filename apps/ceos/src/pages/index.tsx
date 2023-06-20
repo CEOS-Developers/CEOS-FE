989930f415787c49599849a52afb951de1ce7b1c
kongnayeon <seoultownroad@naver.com>
2023-06-20 21:31:20 +0900

    feat: select button admin 디자인 반영

--- apps/ceos/src/pages/index.tsx
+++ apps/ceos/src/pages/index.tsx
@@ -1,4 +1,34 @@
 import { Flex } from '@ceos-fe/ui';
+import { SelectButton } from '../../../../packages/ui/src/components/SelectButton/index';
+import { useForm } from 'react-hook-form';
 export default function Home() {
-  return <Flex direction="row"></Flex>;
+  const { register, watch } = useForm({
+    defaultValues: {
+      title: '',
+      content: '',
+      part: '',
+    },
+  });
+  return (
+    <Flex direction="row">
+      <SelectButton
+        variant="admin"
+        value="기획"
+        webWidth={240}
+        {...register('part')}
+      />
+      <SelectButton
+        variant="admin"
+        value="디자인"
+        webWidth={240}
+        {...register('part')}
+      />
+      <SelectButton
+        variant="admin"
+        value="개발"
+        webWidth={240}
+        {...register('part')}
+      />
+    </Flex>
+  );
 }
