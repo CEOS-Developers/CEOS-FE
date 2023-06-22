ed2542071f0606db12dc07d8bead4e1a9128a84d
Jieun Oh <0909oje@naver.com>
2023-06-22 21:48:33 +0900

    feat: project GET 서버 통신 샘플

--- apps/ceos/src/pages/project/index.tsx
+++ apps/ceos/src/pages/project/index.tsx
@@ -1,7 +1,14 @@
 import { Flex } from '@ceos-fe/ui';
 import { Title } from '@ceos/components/Title';
+import { useEffect } from 'react';
+import { projectApi } from '@ceos-fe/utils';
 
-const Project = () => {
+// any : data Interface 미정
+const Project = ({ data }: { data: any }) => {
+  useEffect(() => {
+    console.log('project 페이지 내 data : ', data);
+  }, [data]);
+
   return (
     <Flex direction="column">
       <Title
@@ -13,6 +20,18 @@ const Project = () => {
       />
     </Flex>
   );
+};
+
+export const getStaticProps = async () => {
+  try {
+    const data = await projectApi.GET_PROJECT({ pageNum: 1, limit: 1000 });
+
+    return {
+      props: { data },
+    };
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export default Project;
