2135bd2e7a84a7fe95715b111f66202b4c6bfca1
Chaeyeon An <dkscodus1204@ewhain.net>
2023-06-24 01:23:47 +0900

    feat: admin layout 컴포넌트 생성

---
+++ apps/admin/src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import Sidebar from '../Sidebar';
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Sidebar />
+      <ChildrenContainer>{children}</ChildrenContainer>
+    </>
+  );
+};
+
+const ChildrenContainer = styled.div`
+  margin-left: 16.5%;
+`;
--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -1,68 +1,57 @@
-import styled from '@emotion/styled';
-import Sidebar from '@admin/components/Sidebar';
 import { Dropdown } from '@admin/components/Dropdown';
 import { useForm } from 'react-hook-form';
-import { theme, Flex } from '@ceos-fe/ui';
+import { Flex, theme } from '@ceos-fe/ui';
 
 export default function Home() {
   const { setValue, watch } = useForm();
 
   return (
-    <>
-      <Sidebar />
-      <LeftContainer>
-        <Flex direction="column">
-          <Dropdown
-            options={[
-              {
-                label: '합격',
-                value: 'pass',
-                background: theme.palette.Admin.Green2,
-                color: theme.palette.Admin.Green1,
-              },
-              {
-                label: '불합격',
-                value: 'nonpass',
-                background: theme.palette.Admin.Red2,
-                color: theme.palette.Admin.Red1,
-              },
-            ]}
-            label="passDropdown"
-            setValue={setValue}
-            value={watch('passDropdown')}
-            placeholder="선택"
-          />
-          <Dropdown
-            options={[
-              {
-                label: '기획',
-                value: 'strategy',
-              },
-              {
-                label: '디자인',
-                value: 'design',
-              },
-              {
-                label: '프론트엔드',
-                value: 'frontend',
-              },
-              {
-                label: '백엔드',
-                value: 'backend',
-              },
-            ]}
-            label="partDropdown"
-            setValue={setValue}
-            value={watch('partDropdown')}
-            placeholder="파트 선택"
-            width={152}
-          />
-        </Flex>
-      </LeftContainer>
-    </>
+    <Flex direction="column">
+      <Dropdown
+        options={[
+          {
+            label: '합격',
+            value: 'pass',
+            background: theme.palette.Admin.Green2,
+            color: theme.palette.Admin.Green1,
+          },
+          {
+            label: '불합격',
+            value: 'nonpass',
+            background: theme.palette.Admin.Red2,
+            color: theme.palette.Admin.Red1,
+          },
+        ]}
+        label="passDropdown"
+        setValue={setValue}
+        value={watch('passDropdown')}
+        placeholder="선택"
+      />
+      <Dropdown
+        options={[
+          {
+            label: '기획',
+            value: 'strategy',
+          },
+          {
+            label: '디자인',
+            value: 'design',
+          },
+          {
+            label: '프론트엔드',
+            value: 'frontend',
+          },
+          {
+            label: '백엔드',
+            value: 'backend',
+          },
+        ]}
+        label="partDropdown"
+        setValue={setValue}
+        value={watch('partDropdown')}
+        placeholder="파트 선택"
+        width={152}
+      />
+    </Flex>
   );
 }
-
-const LeftContainer = styled.div`
-  margin-left: 16.5%;
-`;
