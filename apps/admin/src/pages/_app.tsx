cb6af1b2afceea00c87fa2e579852438872c70bc
Chaeyeon An <dkscodus1204@ewhain.net>
2023-06-24 01:28:27 +0900

    feat: admin layout 컴포넌트 생성

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,15 +1,16 @@
-import "@admin/styles/globals.css";
-import { globalStyle, theme } from "@ceos-fe/ui";
-import { Global, ThemeProvider } from "@emotion/react";
-import type { AppProps } from "next/app";
+import { Layout } from '@admin/components/Layout';
+import '@admin/styles/globals.css';
+import { globalStyle, theme } from '@ceos-fe/ui';
+import { Global, ThemeProvider } from '@emotion/react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <ThemeProvider theme={theme}>
         <Global styles={globalStyle} />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </Layout>
   );
 }
