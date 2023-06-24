e9333a8da16649760c758a8c12956017cbb123d4
juhyojeong <juhyojeong58@gmail.com>
2023-06-24 13:06:43 +0900

    feat: admin - react query 적용

--- packages/utils/src/apis/index.ts
+++ packages/utils/src/apis/index.ts
@@ -1,3 +1,6 @@
 export * from './admin';
 export * from './ceos';
 export * from './axiosConfig';
+export * from './ceos/projectApi';
+export * from './ceos/activityApi';
+export * from './admin/faqApi';
