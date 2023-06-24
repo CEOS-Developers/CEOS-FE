f7cd1debcf96c386c76c7d4466614878d7bf6ab5
juhyojeong <juhyojeong58@gmail.com>
2023-06-24 14:48:53 +0900

    feat: post, patch, delete api 연동

--- packages/utils/src/apis/admin/faqApi.tsx
+++ packages/utils/src/apis/admin/faqApi.tsx
@@ -1,10 +1,33 @@
 import { adminInstance } from '../axiosConfig';
 
 export type CategoryType = 'RECRUIT' | 'ACTIVITY' | 'PART';
+export interface FaqListItemInterface {
+  id: number;
+  category: CategoryType;
+  question: string;
+  answer: string;
+}
 
 export const faqApi = {
   GET_FAQ: async (category: CategoryType) => {
     const response = await adminInstance.get(`/faq?category=${category}`);
+
+    return response.data;
+  },
+  POST_FAQ: async (question: FaqListItemInterface) => {
+    const { id, ...body } = question;
+    const response = await adminInstance.post(`/faq`, body);
+
+    return response.data;
+  },
+  PATCH_FAQ: async (question: FaqListItemInterface) => {
+    const { id, category, ...body } = question;
+    const response = await adminInstance.patch(`/faq/${id}`, body);
+
+    return response.data;
+  },
+  DELETE_FAQ: async (faqId: number) => {
+    const response = await adminInstance.delete(`/faq/${faqId}`);
 
     return response.data;
   },
