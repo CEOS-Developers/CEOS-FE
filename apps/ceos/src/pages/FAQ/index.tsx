06c63b9a92dfde6099a3b10b972da55040eb966c
Jieun Oh <0909oje@naver.com>
2023-06-25 15:50:03 +0900

    feat: 임시 커밋

--- apps/ceos/src/pages/FAQ/index.tsx
+++ apps/ceos/src/pages/FAQ/index.tsx
@@ -1,7 +1,12 @@
 import { Flex } from '@ceos-fe/ui';
 import { Title } from '@ceos/components/Title';
+import { faqApi } from '../../../../../packages/utils/src/apis/ceos/faqApi';
+import { useEffect } from 'react';
 
 const FAQ = () => {
+  // useEffect(() => {
+  //   console.log('FAQ 페이지 내 data : ', data);
+  // }, [data]);
   return (
     <Flex direction="column">
       <Title
@@ -11,5 +16,18 @@ const FAQ = () => {
     </Flex>
   );
 };
+
+// export const getServerSideProps = async () => {
+//   try {
+//     const data = await faqApi.GET_FAQ({ category: 'PART' });
+//     console.log(data);
+
+//     return {
+//       props: data,
+//     };
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 export default FAQ;
