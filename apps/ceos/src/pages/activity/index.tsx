9864943b1aeca590dd67a456538b0c3e1368a3d7
Jieun Oh <0909oje@naver.com>
2023-06-28 20:36:34 +0900

    feat: activity 페이지 구현

--- apps/ceos/src/pages/activity/index.tsx
+++ apps/ceos/src/pages/activity/index.tsx
@@ -1,16 +1,33 @@
-import { Flex } from '@ceos-fe/ui';
+import { Desktop, Flex, Mobile } from '@ceos-fe/ui';
 import { Title } from '@ceos/components/Title';
 import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query';
 import { activityApi } from '@ceos-fe/utils';
 import { ResponseInterface } from '@ceos-fe/utils';
+import { ActivityCard } from '../../../../../packages/ui/src/components/Card/ActivityCard';
 
 // TODO: interface 재정의
-interface ActivityResponse {}
+interface ActivityResponse {
+  activities: { content: string; id: number; imageUrl: string; name: string }[];
+  pageInfo: {
+    pageNum: number;
+    limit: number;
+    totalPages: number;
+    totalElements: number;
+  };
+}
 
 const Activity = () => {
-  const { data, isLoading, isSuccess } = useQuery<
-    ResponseInterface<ActivityResponse>
-  >(['ceos', 'activity'], activityApi.GET_ACTIVITY);
+  const { data, isLoading, isSuccess } = useQuery<{
+    activityData: ResponseInterface<ActivityResponse>;
+  }>(['ceos', 'activity'], async () => {
+    const activityData = await activityApi.GET_ACTIVITY({
+      pageNum: 0,
+      limit: 10000,
+    });
+    return { activityData: activityData };
+  });
+
+  const acitivityList = data?.activityData.data.activities;
 
   return (
     <Flex direction="column">
@@ -21,6 +38,39 @@ const Activity = () => {
           '다양한 활동을 진행하고 있습니다.',
         ]}
       />
+      <Desktop>
+        <Flex direction="column">
+          {acitivityList?.map((_, idx) => {
+            return idx % 3 === 0 ? (
+              <Flex
+                key={`row_${idx}`}
+                justify="start"
+                width={1032}
+                margin="0 0 32px 0"
+                webGap={24}
+              >
+                {acitivityList.slice(idx, idx + 3).map((activity, subIdx) => (
+                  <ActivityCard
+                    key={`activity_${idx}_${subIdx}`}
+                    activityCard={activity}
+                  />
+                ))}
+              </Flex>
+            ) : (
+              <></>
+            );
+          })}
+        </Flex>
+      </Desktop>
+      <Mobile>
+        <Flex direction="column" mobileGap={20}>
+          {acitivityList?.map((activity, idx) => {
+            return (
+              <ActivityCard key={`activity_${idx}`} activityCard={activity} />
+            );
+          })}
+        </Flex>
+      </Mobile>
     </Flex>
   );
 };
@@ -30,7 +80,7 @@ export const getStaticProps = async () => {
     const queryClient = new QueryClient();
 
     await queryClient.prefetchQuery(['ceos', 'activity'], () =>
-      activityApi.GET_ACTIVITY(),
+      activityApi.GET_ACTIVITY({ pageNum: 0, limit: 10000 }),
     );
 
     return {
--- packages/utils/src/apis/ceos/activityApi.tsx
+++ packages/utils/src/apis/ceos/activityApi.tsx
@@ -1,8 +1,16 @@
 import { ceosInstance } from '../axiosConfig';
 
 export const activityApi = {
-  GET_ACTIVITY: async () => {
-    const response = await ceosInstance.get(`/activities`);
+  GET_ACTIVITY: async ({
+    pageNum,
+    limit,
+  }: {
+    pageNum: number;
+    limit: number;
+  }) => {
+    const response = await ceosInstance.get(
+      `/activities?pageNum=${pageNum}&limit=${limit}`,
+    );
 
     return response.data;
   },
