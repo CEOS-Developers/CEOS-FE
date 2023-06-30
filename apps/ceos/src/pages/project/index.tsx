913186ba823a23a627db96ad591b73fe5e2aae59
Jieun Oh <0909oje@naver.com>
2023-06-30 12:04:48 +0900

    feat: Project 페이지 화면 구현

--- apps/ceos/src/pages/project/index.tsx
+++ apps/ceos/src/pages/project/index.tsx
@@ -1,4 +1,10 @@
-import { Flex } from '@ceos-fe/ui';
+import {
+  Desktop,
+  Flex,
+  Mobile,
+  ProjectCard,
+  ProjectCardProps,
+} from '@ceos-fe/ui';
 import { Title } from '@ceos/components/Title';
 import { projectApi } from '@ceos-fe/utils';
 import { ResponseInterface } from '@ceos-fe/utils';
@@ -7,10 +13,10 @@ import {
   dehydrate,
   useInfiniteQuery,
 } from '@tanstack/react-query';
+import Footer from '@ceos/components/Footer';
 
-// TODO: interface 재정의
 interface ProjectResponse {
-  projectBriefInfoVos: any[];
+  projectBriefInfoVos: ProjectCardProps[];
   pageInfo: {
     pageNum: number;
     limit: number;
@@ -32,18 +38,83 @@ const Project = () => {
     },
   );
 
-  console.log(data);
+  const projectList = data?.pages[0].data.projectBriefInfoVos;
+
+  const leftBtn = {
+    title: '더 궁금한 것이 있다면',
+    content: ['자주 묻는 질문', '보러가기'],
+    link: '/FAQ',
+  };
+  const rightBtn = {
+    title: 'CEOS에 참여하고 싶다면',
+    content: ['CEOS 18기', '지원하기'],
+    link: '/recruit',
+  };
 
   return (
-    <Flex direction="column">
-      <Title
-        title="Project"
-        explain={[
-          '신촌 연합 IT 창업동아리 CEOS의',
-          '활동 프로젝트를 소개합니다.',
-        ]}
-      />
-    </Flex>
+    <>
+      <Desktop>
+        <Flex direction="column">
+          <Title
+            title="Project"
+            explain={[
+              '신촌 연합 IT 창업동아리 CEOS의',
+              '활동 프로젝트를 소개합니다.',
+            ]}
+          />
+          <Flex webGap={24}>
+            <Flex
+              direction="column"
+              justify="start"
+              webGap={47}
+              height={(291 * Number(projectList?.length)) / 3}
+            >
+              {projectList?.map((project, idx) =>
+                idx % 3 === 0 ? <ProjectCard projectCard={project} /> : <></>,
+              )}
+            </Flex>
+            <Flex
+              direction="column"
+              justify="start"
+              webGap={47}
+              height={(291 * Number(projectList?.length)) / 3}
+            >
+              {projectList?.map((project, idx) =>
+                idx % 3 === 1 ? <ProjectCard projectCard={project} /> : <></>,
+              )}
+            </Flex>
+            <Flex
+              direction="column"
+              justify="start"
+              webGap={47}
+              height={(291 * Number(projectList?.length)) / 3}
+            >
+              {projectList?.map((project, idx) =>
+                idx % 3 === 2 ? <ProjectCard projectCard={project} /> : <></>,
+              )}
+            </Flex>
+          </Flex>
+          <Footer leftBtn={leftBtn} rightBtn={rightBtn} />
+        </Flex>
+      </Desktop>
+      <Mobile>
+        <Flex direction="column">
+          <Title
+            title="Project"
+            explain={[
+              '신촌 연합 IT 창업동아리 CEOS의',
+              '활동 프로젝트를 소개합니다.',
+            ]}
+          />
+          <Flex direction="column" mobileGap={20} margin="0 0 36px 0">
+            {projectList?.map((project, idx) => (
+              <ProjectCard projectCard={project} />
+            ))}
+          </Flex>
+          <Footer leftBtn={leftBtn} rightBtn={rightBtn} />
+        </Flex>
+      </Mobile>
+    </>
   );
 };
 
