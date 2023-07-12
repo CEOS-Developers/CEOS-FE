e4bb6e3ed501f1ec98b88d2a4a8afa337acf5031
Jieun Oh <0909oje@naver.com>
2023-07-12 23:53:11 +0900

    refactor: apply 인터페이스 분리

---
+++ apps/ceos/src/pages/recruit/apply/interface.tsx
@@ -0,0 +1,43 @@
+import { RecruitApplyValuesInterface } from 'packages/utils';
+import {
+  UseFormHandleSubmit,
+  UseFormRegister,
+  UseFormSetValue,
+  UseFormWatch,
+} from 'react-hook-form';
+
+export interface informationInterface {}
+
+export interface QuestionProps {
+  questionId: number;
+  questionIndex: number;
+  question: string;
+  multiline: boolean;
+  questionDetail: { explaination: string; color: string }[];
+}
+export interface AnswerInterface {
+  questionId: number;
+  answer: string;
+}
+export interface RecruitApplyResponse {
+  commonQuestions: QuestionProps[];
+  productQuestions: QuestionProps[];
+  designQuestions: QuestionProps[];
+  frontendQuestions: QuestionProps[];
+  backendQuestions: QuestionProps[];
+  times: { date: string; durations: string[] }[];
+}
+
+export interface RecruitApplyFormInterface {
+  register: UseFormRegister<RecruitApplyValuesInterface>;
+  watch: UseFormWatch<RecruitApplyValuesInterface>;
+  setValue: UseFormSetValue<RecruitApplyValuesInterface>;
+  handleSubmit: UseFormHandleSubmit<RecruitApplyValuesInterface>;
+  questionList?: RecruitApplyResponse;
+}
+
+export type PartName =
+  | 'productQuestions'
+  | 'designQuestions'
+  | 'frontendQuestions'
+  | 'backendQuestions';
