e4649ca581cd09f3227ab526aed0a50d318e6313
kongnayeon <seoultownroad@naver.com>
2023-07-22 15:39:20 +0900

    fix: 빌드 에러 수정

--- packages/utils/src/apis/axiosConfig.ts
+++ packages/utils/src/apis/axiosConfig.ts
@@ -13,6 +13,3 @@ export const adminInstance = axios.create({
 export const publicInstance = axios.create({
   baseURL: BASE_URL,
 });
-
-adminInstance.defaults.headers.common['Authorization'] =
-  'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzIiwiYXV0aCI6IlJPTEVfUk9PVCIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE2ODk5NTc5MDcsImV4cCI6MTY5MDA0NDMwN30.UGDqrmhyCA7WVM47ZFQ9eqHSddqg4lCtojiGiNQhcvIM9zg3zqxyQZyJPyPMmQY0aJJwQoSdGZlUzH6169nSRw';
