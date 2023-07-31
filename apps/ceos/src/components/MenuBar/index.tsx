f834a1541ddbd704c1dd60c7f4e73c9d95e4bd8d
juhyojeong <juhyojeong58@gmail.com>
2023-07-31 16:16:48 +0900

    fix: 메뉴바 선택상태 추가

--- apps/ceos/src/components/MenuBar/index.tsx
+++ apps/ceos/src/components/MenuBar/index.tsx
@@ -3,6 +3,7 @@ import { Flex, FloatingButton, theme } from '@ceos-fe/ui';
 import { CloseIcon } from '@ceos-fe/ui/src/assets/CloseIcon';
 import Link from 'next/link';
 import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
 
 export interface MenuProps {
   isOpen: boolean;
@@ -12,11 +13,12 @@ export interface MenuProps {
 
 export const MenuBar = (props: MenuProps) => {
   const { isOpen, modalRef, toggleModal } = props;
+  const router = useRouter();
 
   return (
     <div css={backCss} className={isOpen ? 'open' : 'close'}>
       <div
-        css={positionCss(isOpen)}
+        css={positionCss()}
         ref={modalRef}
         className={isOpen ? 'open' : 'close'}
       >
@@ -28,16 +30,40 @@ export const MenuBar = (props: MenuProps) => {
           />
           <div css={contentCss}>
             <p>
-              <CustomLink href="/project">PROJECT</CustomLink>
+              <CustomLink
+                href="/project"
+                onClick={toggleModal}
+                isSelected={router.pathname === '/project'}
+              >
+                PROJECT
+              </CustomLink>
             </p>
             <p>
-              <CustomLink href="/activity">ACTIVITY</CustomLink>
+              <CustomLink
+                href="/activity"
+                onClick={toggleModal}
+                isSelected={router.pathname === '/activity'}
+              >
+                ACTIVITY
+              </CustomLink>
             </p>
             <p>
-              <CustomLink href="/FAQ">FAQ</CustomLink>
+              <CustomLink
+                href="/FAQ"
+                onClick={toggleModal}
+                isSelected={router.pathname === '/FAQ'}
+              >
+                FAQ
+              </CustomLink>
             </p>
             <p>
-              <CustomLink href="/recruit">RECRUIT</CustomLink>
+              <CustomLink
+                href="/recruit"
+                onClick={toggleModal}
+                isSelected={router.pathname === '/recruit'}
+              >
+                RECRUIT
+              </CustomLink>
             </p>
           </div>
           <Flex align="flex-end" margin="0px 0px 100px 0px">
@@ -62,7 +88,7 @@ export const backCss = () => css`
   }
 `;
 
-export const positionCss = (isOpen: boolean) => css`
+export const positionCss = () => css`
   height: 100%;
   right: -90%;
   top: 0;
@@ -76,7 +102,7 @@ export const positionCss = (isOpen: boolean) => css`
 `;
 
 export const contentCss = () => css`
-  dispaly: flex;
+  display: flex;
   flex-direction: column;
   typo: ${theme.typo.Mobile.Heading1_Kor};
   color: ${theme.palette.Gray6};
@@ -91,7 +117,9 @@ export const contentCss = () => css`
   }
 `;
 
-export const CustomLink = styled(Link)`
+export const CustomLink = styled(Link)<{ isSelected?: boolean }>`
   text-decoration: none;
   color: inherit;
+  color: ${({ isSelected }) =>
+    isSelected ? theme.palette.Green : theme.palette.Gray4};
 `;
