bb12e5ac2b3bc6f34f6c95ae07c4df9b1352379a
kongnayeon <seoultownroad@naver.com>
2023-08-02 00:04:42 +0900

    feat: 파비콘 추가

--- apps/admin/src/pages/_document.tsx
+++ apps/admin/src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="admin-favicon.svg" />
         <link
           href="https://fonts.cdnfonts.com/css/gilroy-bold"
           rel="stylesheet"
