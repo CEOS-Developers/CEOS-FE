21bfc376fe77b0dace4a92abffe4c2c3646d301d
오지은 <0909oje@naver.com>
2023-08-02 13:24:30 +0900

    fix: recruit 추가

---
+++ apps/ceos/src/components/recruit/Common.tsx
@@ -0,0 +1,40 @@
+import { Desktop, Flex, Mobile, Text, TextField } from '@ceos-fe/ui';
+import { RowLine, Section } from '../../pages/recruit/apply';
+import { CustomTextField, Explain, Question, QuestionFlex } from './style';
+import { RecruitApplyFormInterface } from './interface';
+
+interface CommonProps {
+  register: RecruitApplyFormInterface['register'];
+  questionList?: RecruitApplyFormInterface['questionList'];
+}
+
+const Common = ({ register, questionList }: CommonProps) => {
+  return (
+    <>
+      <Section>
+        <Text webTypo="Heading3" paletteColor="Blue">
+          공통 질문
+        </Text>
+        {questionList?.commonQuestions.map((ques, idx) => (
+          <QuestionFlex key={ques.questionId}>
+            <CustomTextField
+              {...register(`commonAnswers.${idx}.answer`)}
+              label={`${idx + 1}. ${ques.question}`}
+              multiline={ques.multiline}
+              width={856}
+              helperText={ques.questionDetail.map((detail) => {
+                return {
+                  type: detail.color === 'gray' ? 'normal' : 'important',
+                  text: detail.explaination,
+                };
+              })}
+            />
+          </QuestionFlex>
+        ))}
+      </Section>
+      <RowLine />
+    </>
+  );
+};
+
+export default Common;
