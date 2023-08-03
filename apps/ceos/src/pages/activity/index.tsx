ac7fe3e2ec69c431b2d8bd4b56f8a5d587df8416
kongnayeon <seoultownroad@naver.com>
2023-08-04 00:40:36 +0900

    fix: activity 페이지 모바일 대문자 수정

--- apps/ceos/src/pages/activity/index.tsx
+++ apps/ceos/src/pages/activity/index.tsx
@@ -71,7 +71,7 @@ const Activity = () => {
       <Mobile>
         <Flex direction="column" data-section="White">
           <Title
-            title="Activity"
+            title="ACTIVITY"
             explain={[
               'ceos에서는 it 창업과 관련된',
               '다양한 활동을 진행하고 있습니다.',
