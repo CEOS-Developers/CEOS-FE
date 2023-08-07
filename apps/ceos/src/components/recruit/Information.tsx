f2a52a7a36742d26d946305f5ffd503f971ecc5d
오지은 <0909oje@naver.com>
2023-08-07 14:23:54 +0900

    fix: 지원서 gap

--- apps/ceos/src/components/recruit/Information.tsx
+++ apps/ceos/src/components/recruit/Information.tsx
@@ -27,126 +27,126 @@ const Information = ({ register, setValue }: InformationProps) => {
   const univ = ['연세대학교', '서강대학교', '이화여자대학교', '홍익대학교'];
 
   return (
-    <Flex direction="column">
-      <Flex direction="column" webGap={36} mobileGap={28} width={680}>
+    <Flex direction="column" webGap={36} mobileGap={28}>
+      <CustomFlex>
+        <QuestionFlex>
+          <CustomTextField
+            label="이름"
+            {...register('name')}
+            webWidth="328px"
+          />
+        </QuestionFlex>
+        <QuestionFlex>
+          <Question>성별</Question>
+          <Flex webGap={12} mobileGap={14}>
+            <SelectButton
+              variant="ceos"
+              value="남성"
+              webWidth={158}
+              {...register('gender')}
+            />
+            <SelectButton
+              variant="ceos"
+              value="여성"
+              webWidth={158}
+              {...register('gender')}
+            />
+          </Flex>
+        </QuestionFlex>
+      </CustomFlex>
+      <CustomFlex>
+        <CustomTextField
+          label="생년월일"
+          {...register('birth')}
+          placeholder="yyyy.mm.dd"
+          webWidth="328px"
+        />
+        <CustomTextField
+          label="이메일"
+          {...register('email')}
+          webWidth="328px"
+        />
+      </CustomFlex>
+      <CustomFlex>
+        <CustomTextField
+          label="전화번호"
+          {...register('phoneNumber')}
+          placeholder="000-0000-0000"
+          webWidth="328px"
+        />
+      </CustomFlex>
+      <Line />
+      <Flex direction="column" webGap={36} mobileGap={28}>
         <CustomFlex>
           <QuestionFlex>
-            <CustomTextField
-              label="이름"
-              {...register('name')}
-              webWidth="328px"
-            />
-          </QuestionFlex>
-          <QuestionFlex>
-            <Question>성별</Question>
-            <Flex webGap={12} mobileGap={14}>
-              <SelectButton
-                variant="ceos"
-                value="남성"
-                webWidth={158}
-                {...register('gender')}
-              />
-              <SelectButton
-                variant="ceos"
-                value="여성"
-                webWidth={158}
-                {...register('gender')}
-              />
-            </Flex>
+            <Question>재학 중인 학교</Question>
+            <Grid>
+              {univ.map((uni) => (
+                <SelectButton
+                  variant="ceos"
+                  value={uni}
+                  webWidth={161}
+                  {...register('university')}
+                />
+              ))}
+            </Grid>
+            <Explain>*학부생, 대학원생, 휴학생 모두 해당</Explain>
           </QuestionFlex>
         </CustomFlex>
         <CustomFlex>
           <CustomTextField
-            label="생년월일"
-            {...register('birth')}
-            placeholder="yyyy.mm.dd"
+            label="전공(학과)"
+            {...register('major')}
+            helperText={[
+              {
+                type: 'normal',
+                text: '*복수전공 및 부전공까지 포함하여 입력',
+              },
+              {
+                type: 'normal',
+                text: 'ex. 컴퓨터공학과 / 경영학과',
+              },
+            ]}
             webWidth="328px"
           />
           <CustomTextField
-            label="이메일"
-            {...register('email')}
+            label="졸업까지 남은 학기 수"
+            {...register('semestersLeftNumber')}
+            helperText={[
+              {
+                type: 'normal',
+                text: '*ex. 2',
+              },
+            ]}
             webWidth="328px"
           />
         </CustomFlex>
-        <CustomFlex>
-          <CustomTextField
-            label="전화번호"
-            {...register('phoneNumber')}
-            placeholder="000-0000-0000"
-            webWidth="328px"
-          />
-        </CustomFlex>
-        <Line />
-        <Flex direction="column" webGap={36} mobileGap={28}>
-          <CustomFlex>
-            <QuestionFlex>
-              <Question>재학 중인 학교</Question>
-              <Grid>
-                {univ.map((uni) => (
-                  <SelectButton
-                    variant="ceos"
-                    value={uni}
-                    webWidth={161}
-                    {...register('university')}
-                  />
-                ))}
-              </Grid>
-              <Explain>*학부생, 대학원생, 휴학생 모두 해당</Explain>
-            </QuestionFlex>
-          </CustomFlex>
-          <CustomFlex>
-            <CustomTextField
-              label="전공(학과)"
-              {...register('major')}
-              helperText={[
-                {
-                  type: 'normal',
-                  text: '*복수전공 및 부전공까지 포함하여 입력',
-                },
-                {
-                  type: 'normal',
-                  text: 'ex. 컴퓨터공학과 / 경영학과',
-                },
-              ]}
-              webWidth="328px"
-            />
-            <CustomTextField
-              label="졸업까지 남은 학기 수"
-              {...register('semestersLeftNumber')}
-              helperText={[
-                {
-                  type: 'normal',
-                  text: '*ex. 2',
-                },
-              ]}
-              webWidth="328px"
-            />
-          </CustomFlex>
-        </Flex>
+      </Flex>
 
-        <Line />
-        <CustomFlex>
-          <QuestionFlex>
-            <Question>CEOS OT 날짜는?</Question>
-            <DatePicker
-              onChange={(date: Date | null) =>
-                setValue('otDate', changeDate(date))
-              }
-            />
-          </QuestionFlex>
-          <QuestionFlex>
-            <Text webTypo="Label3">CEOS 데모데이 날짜는?</Text>
-            <DatePicker
-              onChange={(date: Date | null) =>
-                setValue('demodayDate', changeDate(date))
-              }
-            />
-          </QuestionFlex>
-        </CustomFlex>
-        <Flex direction="column" align="start" webGap={8} mobileGap={14}>
-          <Text webTypo="Label3">
+      <Line />
+      <CustomFlex>
+        <QuestionFlex>
+          <Question>CEOS OT 날짜는?</Question>
+          <DatePicker
+            onChange={(date: Date | null) =>
+              setValue('otDate', changeDate(date))
+            }
+          />
+        </QuestionFlex>
+        <QuestionFlex>
+          <Question>CEOS 데모데이 날짜는?</Question>
+          <DatePicker
+            onChange={(date: Date | null) =>
+              setValue('demodayDate', changeDate(date))
+            }
+          />
+        </QuestionFlex>
+      </CustomFlex>
+      <CustomFlex>
+        <QuestionFlex>
+          <Question>
             이번 학기 세오스 활동 외 어떤 활동을 하는지 간략히 적어주세요.
-          </Text>
+          </Question>
           <CustomTextField
             {...register('otherActivities')}
             helperText={[
@@ -157,9 +157,9 @@ const Information = ({ register, setValue }: InformationProps) => {
             ]}
             webWidth="680px"
           />
-        </Flex>
-        <Line />
-      </Flex>
+        </QuestionFlex>
+      </CustomFlex>
+      <Line />
     </Flex>
   );
 };
