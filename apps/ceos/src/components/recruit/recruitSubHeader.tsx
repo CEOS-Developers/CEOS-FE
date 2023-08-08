cdc18555764815f1d7f9a537397c54f9ddb87dbf
kongnayeon <seoultownroad@naver.com>
2023-08-09 00:10:02 +0900

    fix: 리크루트 페이지 간격 수정

--- apps/ceos/src/components/recruit/recruitSubHeader.tsx
+++ apps/ceos/src/components/recruit/recruitSubHeader.tsx
@@ -3,7 +3,7 @@ import {
   RecruitTextCss,
   RecruitBgText,
 } from '@ceos/styles/recruit';
-import { Button, Text } from '@ceos-fe/ui';
+import { Button, Flex, Text } from '@ceos-fe/ui';
 import { css } from '@emotion/react';
 import { useModal } from '@ceos-fe/utils';
 import { CheckModal } from '@ceos/components/Modals/checkModal';
@@ -77,58 +77,64 @@ export const RecruitSubHeader = ({
               </div>
             </Link>
           ) : (
-            <div>
+            <>
               {resultDateDoc <= curDate && curDate < resultDateFinal ? (
-                <div>
-                  <div style={{ width: '100%', display: 'flex' }}>
-                    <Button
-                      variant="glass"
-                      webWidth={249}
-                      css={BtnCss}
-                      onClick={() => {
-                        toggleModal();
-                        setStep('서류');
-                      }}
-                    >
-                      서류 합격 여부 확인하기
-                    </Button>
-                  </div>
-                </div>
+                <Flex
+                  css={css`
+                    padding: 0px 22px 0px 22px;
+                  `}
+                >
+                  <Button
+                    variant="glass"
+                    webWidth={249}
+                    css={BtnCss}
+                    onClick={() => {
+                      toggleModal();
+                      setStep('서류');
+                    }}
+                  >
+                    서류 합격 여부 확인하기
+                  </Button>
+                </Flex>
               ) : (
-                <div>
+                <>
                   {resultDateFinal <= curDate && curDate <= endDate ? (
-                    <div>
-                      <div style={{ width: '100%', display: 'flex' }}>
-                        <Button
-                          variant="glass"
-                          webWidth={249}
-                          css={BtnCss}
-                          onClick={() => {
-                            toggleModal();
-                            setStep('최종');
-                          }}
-                        >
-                          최종 합격 여부 확인하기
-                        </Button>
-                      </div>
-                    </div>
+                    <Flex
+                      css={css`
+                        padding: 0px 22px 0px 22px;
+                      `}
+                    >
+                      <Button
+                        variant="glass"
+                        webWidth={249}
+                        css={BtnCss}
+                        onClick={() => {
+                          toggleModal();
+                          setStep('최종');
+                        }}
+                      >
+                        최종 합격 여부 확인하기
+                      </Button>
+                    </Flex>
                   ) : (
-                    <div>
-                      <div style={{ width: '100%', display: 'flex' }}>
-                        <Button
-                          variant="glass"
-                          webWidth={234}
-                          css={BtnCss}
-                          disabled
-                        >
-                          지원 기간이 아닙니다.
-                        </Button>
-                      </div>
-                    </div>
+                    <Flex
+                      css={css`
+                        padding: 0px 22px 0px 22px;
+                      `}
+                    >
+                      <Button
+                        variant="glass"
+                        webWidth={234}
+                        css={BtnCss}
+                        disabled
+                      >
+                        지원 기간이 아닙니다.
+                      </Button>
+                    </Flex>
                   )}
-                </div>
+                </>
               )}
-            </div>
+            </>
           )}
         </>
       </div>
@@ -153,8 +159,10 @@ export const BtnCss = css`
   box-sizing: border-box;
 
   @media (max-width: 1023px) {
-    margin-top: 90px;
-    width: 346px;
+    box-sizing: content-box;
+    margin-top: 108px;
     height: 59px;
+    max-width: 766px;
+    width: 100%;
   }
 `;
