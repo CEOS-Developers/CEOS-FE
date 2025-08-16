06d672d5ebf20a4fc80acfe30c3d7d331697b39b
yyj0917 <yyj0917@yonsei.ac.kr>
2025-08-16 23:42:06 +0900

    fix : recruitSubHeader Box 20 -> 22기로 변경

--- apps/ceos/src/components/Footer/index.tsx
+++ apps/ceos/src/components/Footer/index.tsx
@@ -37,7 +37,7 @@ const Footer = ({
               </CustomLink>
             </Flex>
             <Text paletteColor="White" webTypo="Label3">
-              © 2015-2024 CEOS ALL RIGHTS RESERVED.
+              © 2015-2025 CEOS ALL RIGHTS RESERVED.
             </Text>
           </Flex>
         </Container>
--- apps/ceos/src/components/recruit/recruitSubHeader.tsx
+++ apps/ceos/src/components/recruit/recruitSubHeader.tsx
@@ -105,7 +105,7 @@ export const RecruitSubHeader = ({
                       toggleEmailModal();
                     }}
                   >
-                    20기 모집 알림받기
+                    22기 모집 알림받기
                   </Button>
                 </div>
               </div>
